123e9520420a2dad13392ae10d87ce8669cb87d8
mateusz14 <36890661+mateusz14@users.noreply.github.com>
2018-12-14 11:36:56 +0100

    Update quarterListItem.jsx

--- src/components/quarters/others/quarterListItem/quarterListItem.jsx
+++ src/components/quarters/others/quarterListItem/quarterListItem.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import './quarterListItem.scss';
 import QuarterDetalilsItem from '../quarterDetailsItem/quarterDetailsItem';
 
-const quarterListItem = ({index, item, clickItemFunction, currentWatchedItemId, subHeader, reactivate, 
+const quarterListItem = ({index, item, clickItemFunction, currentWatchedItemId, subHeader, reactivate,
     deleteTranslation, conduct, quarter, QuarterDeletedPrompt, isDetailItemFromEmployeeDetails, answers, forQuarter,
     connector, inYear, doneQuarter, incomingQuarter}) => (
     <div className={`single-quarter ${item.isDeleted ? "deleted-quarter" : ""} ${index === currentWatchedItemId ? "current-watched-item" : ""}`} onClick={e => clickItemFunction(e)}>
         <p>
-            <i title="Osoba przeprowadzająca rozmowe" className="fa fa-user"></i>
-            <span title="Osoba przeprowadzająca rozmowe">{item.questionerId}</span>
+            <i title="Osoba przeprowadzająca rozmowę" className="fa fa-user"></i>
+            <span title="Osoba przeprowadzająca rozmowę">{item.questionerId}</span>
             <span className="span-light">{item.isTaken ? subHeader : conduct}</span>
             {item.isTaken && !isDetailItemFromEmployeeDetails &&
                 <i title="Pobierz rozmowę w formacie .doc" onClick={e => clickItemFunction(e, "generateDoc")} className="fa fa-file-alt"></i>
@@ -17,16 +17,17 @@ const quarterListItem = ({index, item, clickItemFunction, currentWatchedItemId, 
         </p>
         <p>
             <span className="clickable">
-                 {forQuarter} {item.quarter} {quarter} {item.year} {inYear} <b>{connector}</b> 
+
+                 {forQuarter} {item.quarter} {quarter} <b>{connector}</b> {item.year} {inYear}
             </span>
             <span className="span-light" title={item.isTaken ? "Data przeprowadzenia rozmowy" : "Planowana data odbycia rozmowy"}>
                 <i className="fa fa-calendar-alt"></i>{item.isTaken ? item.aswerQuestionDate : item.plannedTalkDate}
             </span>
         </p>
         <p className={(isDetailItemFromEmployeeDetails && index === currentWatchedItemId) ? "border-paragraph" : ""}>
-            {isDetailItemFromEmployeeDetails && item.isTaken && 
+            {isDetailItemFromEmployeeDetails && item.isTaken &&
                 <span className="details-span" onClick={e => clickItemFunction(e, "expendDetails")}>
-                    <i className={`fa fa-chevron-down ${index === currentWatchedItemId ? "rot-chev" : ""}`}></i>    
+                    <i className={`fa fa-chevron-down ${index === currentWatchedItemId ? "rot-chev" : ""}`}></i>
                 </span>
             }
             {isDetailItemFromEmployeeDetails ||
@@ -38,13 +39,13 @@ const quarterListItem = ({index, item, clickItemFunction, currentWatchedItemId, 
                 <QuarterDetalilsItem item={answer} key={index}/>
             ))
         }
-        {item.isDeleted && 
+        {item.isDeleted &&
             <div className="backdrop-prompt">
                 <p>{QuarterDeletedPrompt}</p>
                 <span onClick={e => clickItemFunction(e, "reactivate")}>{reactivate}</span>
             </div>
         }
-        
+
     </div>
 );
 export default quarterListItem;
